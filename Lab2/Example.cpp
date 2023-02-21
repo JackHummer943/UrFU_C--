ec2d1697c155d8308d22806019c90c470a436114
evg_ser1997@mail.ru <evg_ser1997@mail.ru>
2023-02-21 14:56:09 +0300

    1.2

--- Lab2/Example.cpp
+++ Lab2/Example.cpp
@@ -5,15 +5,20 @@
 void main()
 {
   float m[N][N];
-  int i, j;
-  for (i = 0; i < N; i++)
-    for (j = 0; j < N; j++)
-      m[i][j] = rand() / 10.;
+  float minVal = m[0][0];
   for (i = 0; i < N; i++)
   {
-    for (j = -; j < N; j++)
-      count << setw(8) << setprecision(5) << m[i][j];
-    count << endl;
-    getch();
-  }
-}
+    for (j = 0; j < N; j++)
+    {
+      if
+
+          m
+        [i][j] = rand() / 10.;
+      for (i = 0; i < N; i++)
+      {
+        for (j = -; j < N; j++)
+          count << setw(8) << setprecision(5) << m[i][j];
+        count << endl;
+        getch();
+      }
+    }
